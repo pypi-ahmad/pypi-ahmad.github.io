33f3fc8b0c31259ebc52bbfccdd9bb9626ab12f9
Ahmad Mujtaba <72138709+pypi-ahmad@users.noreply.github.com>
2026-01-30 01:40:28 +0530

    fix(cursor): use RGB color format and high z-index for visibility

--- src/App.js
+++ src/App.js
@@ -24,12 +24,32 @@ function App() {
         <GlobalStyles />
         {useCursor && (
           <AnimatedCursor
-            color={themes[theme].secondaryText}
+            color="220, 90, 90"
             innerSize={8}
-            outerSize={25}
-            outerScale={2}
+            outerSize={35}
             innerScale={0.7}
+            outerScale={1.5}
+            outerAlpha={0.3}
             trailingSpeed={8}
+            innerStyle={{
+              zIndex: 9999,
+            }}
+            outerStyle={{
+              zIndex: 9999,
+            }}
+            clickables={[
+              "a",
+              'input[type="text"]',
+              'input[type="email"]',
+              'input[type="number"]',
+              'input[type="submit"]',
+              'input[type="image"]',
+              "label[for]",
+              "select",
+              "textarea",
+              "button",
+              ".link",
+            ]}
           />
         )}
         <div>
